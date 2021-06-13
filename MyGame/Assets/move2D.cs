c3587ff95e4db16b38f08412711787a7bb79d564
Abhishekrawat <75733415+7951abhishek@users.noreply.github.com>
2021-06-13 11:31:46 +0530

    cube game using C# and UNITY

---
+++ MyGame/Assets/move2D.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class move2D : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+    public float jumpForce = 5f;
+    public float moveSpeed = 10f;
+
+    public Animator animator;
+    private bool facingRight = true;
+
+
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        if (Input.GetAxis("Horizontal") < 0 && facingRight)
+        {
+            flip();
+
+        }
+        if (Input.GetAxis("Horizontal") > 0 && !facingRight)
+        {
+            flip();
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+
+
+    }
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+}
